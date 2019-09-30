6b51d09b7242dfee4e7ddf8ee8511a5f373705b8
Arber Alaj <Arber.Alaj@ksb-sg.ch>
2019-09-30 11:17:56 +0200

    Add project files.

---
+++ IBAN/ConsoleHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBAN
+{
+    public class ConsoleHelper
+    {
+        public static String input()
+        {
+            while(true)
+            {
+
+            int value;
+            int min = 21;
+            int max = 23;
+            String input = Console.ReadLine();
+
+            int.TryParse(input, out value);
+
+            if(value < min && value > max)
+            {
+                Console.WriteLine("Geben Sie eine gültige Zahl ein:");
+                Console.ReadLine();
+                return input;
+            }
+
+            else
+            {
+                Console.WriteLine("\nIhre Eingabe ist korrekt.");
+            }
+                
+            }
+           
+        }
+        
+    }
+}
---
+++ IBAN/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IBAN
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        public void Run()
+        {
+            Console.WriteLine("IBAN-Check");
+
+            Console.WriteLine("\nWählen Sie bitte Ihr gewünsten Bereich: ");
+            Console.WriteLine("\n[a] IBAN-Nummer prüfen");
+            Console.WriteLine("[b] Was ist IBAN?");
+
+            var hauptmenu = Console.ReadLine();
+
+            switch(hauptmenu)
+            {
+                case "a":
+                    Console.Clear();
+                    Console.WriteLine("\nGeben Sie Ihre IBAN-Nummer an:");
+                    Console.ReadLine();
+                    break;
+
+                case "b":
+                    Console.Clear();
+                    Console.WriteLine("Die IBAN ist die Nummer, die Sie auf Ihrer Bankkarte finden.");
+                    Console.WriteLine("Beispiel: CH31 8123 9000 0012 4568 9");
+                    Console.Clear();
+                    return;
+            }
+
+            ConsoleHelper.input();
+        }
+     }
+  }
